9a51e8e84db38c02350b072d696d2c6c82479fcd
ankur-srivastava <ankurlkw@gmail.com>
2018-09-14 09:26:53 +0530

    Added Routing

--- app.js
+++ app.js
@@ -5,8 +5,23 @@ const http = require('http');
 const app = express();
 
 app.use(logger('short'));
+
+app.get('/', (request, response)=>{
+    response.end('Welcome');
+});
+
+app.get('/about', (request, response)=>{
+    response.end('About');
+});
+
+app.get('/hello/:who', (request, response)=>{
+    const who = request.params.who;
+    response.end(`Hello ${who}`);
+});
+
 app.use((request, response) => {
-    response.send('Hello Ankur');
+    response.statusCode = 404;
+    response.end('404');
 });
 
 http.createServer(app).listen(3000);
